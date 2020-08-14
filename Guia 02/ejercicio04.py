6dc50080b614ca6b23d6b6c919733cf69430899f
Carolin <mamaniccarolina@gmail.com>
2020-08-14 13:34:54 -0300

    Submitting some excercises from the Activity Guide N°2

--- Guia 02/ejercicio04.py
+++ Guia 02/ejercicio04.py
@@ -1,2 +1,12 @@
 """Ejercicio 4:
 Ingresar 6 números por teclado, preferentemente enteros, ordenarlos de manera creciente e imprimirlos por pantalla."""
+
+numeros = []
+i = 0
+
+while i < 6:
+    numero = int(input("Ingrese un numero por favor: "))
+    numeros.append(numero)
+    i += 1
+
+print(numeros)
